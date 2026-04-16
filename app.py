8b9ae43f8db944c8c9cfe9d908494e78f9b0b142
Warsut <kaylin367@gmail.com>
2026-04-16 14:30:41 -0400

    Completed assignment

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
-# app = Flask(__name__, 
-#             template_folder='.',
-#             static_folder='./')
+app = Flask(__name__, 
+             template_folder='./templates',
+             static_folder='./dist/assets')
 
 CORS(app)
 
@@ -102,4 +102,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(debug=True,host="0.0.0.0",port=5002)
